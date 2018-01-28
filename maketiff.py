c06255501a6cb24eac0809f441401fa9fd70b348
djkapner <daniel.j.kapner@gmail.com>
2018-01-28 13:38:03 -0800

    improved test pattern

--- maketiff.py
+++ maketiff.py
@@ -5,35 +5,33 @@ from PIL import Image
 #width and height of final image
 w = 3840
 h = 3840
-
-#create 1/4 sub-image for preserving x/y reflection symmetry in final image
-w4 = w/2
-h4 = h/2
-pattern = np.zeros((h4,w4)).astype('uint8')
-
-#make some horizontal lines
-nlines = 10
-wsmall = 4
-wstep = 2
-centerstart = 10 #pixels from edge for first line
-for i in np.arange(nlines):
-    wid = wsmall+i*wstep
-    c = centerstart+i*h4/(nlines)
-    pattern[(c-wid/2):(c+wid/2),:]=255
-
-#reflect around 45deg to make x/y symmetry
-pattern = pattern + np.rot90(pattern,3)
+n=16
+overlap = w/n
 
 full = np.zeros((h,w)).astype('uint8')
+
+#make some lines on top and left edge
+lw = 4
+nlines = 8
+space = (overlap-nlines*lw)/nlines-1
+for i in np.arange(nlines+1):
+    edge = (i+1)*space
+    full[0:2*overlap,edge:edge+lw] = 255
+    full[edge:edge+lw,0:2*overlap] = 255
+for i in np.arange(1,n/2):
+    full[0:2*overlap,i*2*overlap:(i+1)*2*overlap] = full[0:2*overlap,0:2*overlap]
+
+#reflect top edge to left side
+full = full + np.rot90(np.fliplr(full),1)
+
+#make the bottom overlap pixels exactly match the top overlap pixels
+full[(h-overlap):h,:] = full[0:overlap,:]
+#make the right overlap pixels exactly match the left overlap pixels
+full[:,(w-overlap):w] = full[:,0:overlap]
+
 #make mark to break the symmetries
 full[1100:1920,1100:1920] = 255
 full[1200:1920,1200:1920] = 0
-
-order = [1,0,2,3]
-for i in np.arange(2):
-    for j in np.arange(2):
-        full[(i*h4):((i+1)*h4),(j*w4):((j+1)*w4)] = full[(i*h4):((i+1)*h4),(j*w4):((j+1)*w4)]+np.rot90(pattern,order[i*2+j])
-
 
 p = Image.fromarray(full)
 p.save('test_pattern.tif')
@@ -41,6 +39,13 @@ p.save('test_pattern.tif')
 plt.figure(1)
 plt.clf()
 plt.subplot(1,2,1)
-plt.imshow(pattern,cmap='gray_r',interpolation='nearest')
-plt.subplot(1,2,2)
 plt.imshow(full,cmap='gray_r',interpolation='nearest')
+plt.subplot(1,2,2)
+plt.imshow(full,cmap='gray_r',interpolation='nearest',extent=[0,w,h,0])
+plt.imshow(full,cmap='gray_r',interpolation='nearest',extent=[w-overlap,2*w-overlap,h,0])
+plt.imshow(full,cmap='gray_r',interpolation='nearest',extent=[0,w,2*h-overlap,h-overlap])
+plt.imshow(full,cmap='gray_r',interpolation='nearest',extent=[w-overlap,2*w-overlap,2*h-overlap,h-overlap])
+
+plt.gca().set_xlim(0,2*w)
+plt.gca().set_ylim(2*h,0)
+

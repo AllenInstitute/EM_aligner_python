fa16a5a99a6c7abc3986fd7434717d559847e7b1
Dan Kapner <danielk@.alleninstitute.org>
2018-06-04 08:56:50 -0700

    some updates to CheckResiduals

--- EMaligner/qctools/CheckResiduals.py
+++ EMaligner/qctools/CheckResiduals.py
@@ -1,130 +1,122 @@
-from pymongo import MongoClient
 import numpy as np
 import renderapi
 import argschema
 from .. EM_aligner_python_schema import *
-from .. EMaligner import make_dbconnection,get_matches
-import time
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib.collections import LineCollection
 import matplotlib.pyplot as plt
 import mpl_scatter_density
-import subprocess
 
-def transform(pt,tr):
-    #apply an affine transformation
-    newpt = np.zeros_like(pt)
-    newpt[0,:] = tr.M[0,0]*pt[0,:]+tr.M[0,1]*pt[1,:]+tr.M[0,2]
-    newpt[1,:] = tr.M[1,0]*pt[0,:]+tr.M[1,1]*pt[1,:]+tr.M[1,2]
-    return newpt
 
-def compute_residuals(tids,tforms,matches,nsub=-1):
-    xya = []
-    xyd = [] 
+def transform_pq(tspecs,matches):
+    p = []
+    q = []
+    p_transf = []
+    q_transf = [] 
+    tids = []
+    for ts in tspecs:    
+        tids.append(np.array([x.tileId for x in ts]))
+    tsp_ind0=0
+    tsp_ind1=1
+    if len(tspecs)==1:
+        #montage
+        tsp_ind1=0
+
     for match in matches:
-        #cycle through each item, find the appropriate transforms, and apply the transform
-        sind1 = np.argwhere(tids[0]==match['pId'])
-        sind2 = np.argwhere(tids[1]==match['qId'])
-        if (len(sind1)>0) & (len(sind2)>0):
+        #cycle through each item, find and apply the appropriate transforms
+        sind0 = np.argwhere(tids[tsp_ind0]==match['pId'])
+        sind1 = np.argwhere(tids[tsp_ind1]==match['qId'])
+        if (len(sind0)>0) & (len(sind1)>0):
+            sind0 = sind0[0][0]
             sind1 = sind1[0][0]
-            sind2 = sind2[0][0]
-            p = np.array(match['matches']['p'])
-            q = np.array(match['matches']['q'])
-            npts = p.shape[1]
+            ip = np.flipud(np.rot90(np.array(match['matches']['p'])))
+            iq = np.flipud(np.rot90(np.array(match['matches']['q'])))
 
-            #filter the point matches, just to cut down the numbers
-            subind = np.arange(npts)
-            if (nsub!=-1)&(nsub<npts):
-            #currently only random nsub chosen
-                subind = np.random.choice(subind,nsub)
-              
-            pxy = transform(p[:,subind],tforms[0][sind1])
-            qxy = transform(q[:,subind],tforms[1][sind2])
+            ip_transf = tspecs[tsp_ind0][sind0].tforms[-1].tform(ip)
+            iq_transf = tspecs[tsp_ind1][sind1].tforms[-1].tform(iq)
 
-            thind = np.arange(pxy.shape[1])
+            p.append(ip)
+            q.append(iq)
+            p_transf.append(ip_transf)
+            q_transf.append(iq_transf)
 
-            #append onto the outputs
-            if thind.size>0:
-               xya.append(0.5*(pxy[:,thind]+qxy[:,thind]))
-               xyd.append(pxy[:,thind]-qxy[:,thind])
-
-    return [np.block(xya),np.block(xyd)]
+    return [p,q,p_transf,q_transf]
 
 class CheckResiduals(argschema.ArgSchemaParser):
     default_schema = EMA_PlotSchema
 
     def run(self):
-        if self.args['z1'] > self.args['z2']:
-            tmp = self.args['z1']
-            self.args['z1'] = self.args['z2']
-            self.args['z2'] = tmp
-        self.compute_values()
+        self.compute_residuals()
+        self.cmap = plt.cm.plasma_r
         if self.args['plot']:
             self.make_plots()
     
-    def compute_values(self):
-        self.cmap = plt.cm.plasma_r
-        self.args['output_stack']['db_interface']='render'
-        z1 = self.args['z1']
-        z2 = self.args['z2']
-        stack_dbconnection = make_dbconnection(self.args['output_stack'])
-        dbconnection = make_dbconnection(self.args['pointmatch'])
+    def compute_residuals(self):
+        stack_dbconnection = renderapi.connect(**self.args['output_stack'])
+        match_dbconnection = renderapi.connect(**self.args['pointmatch'])
        
-        #get the tilespecs, ids, and transforms
-        tids = []
-        tforms = []
-        for z in [z1,z2]:
-            tspecs = renderapi.tilespec.get_tile_specs_from_z(self.args['output_stack']['name'],int(float(z)),render=stack_dbconnection)
-            tids.append([])
-            tforms.append([])
-            for ts in tspecs:
-                tids[-1].append(ts.tileId)
-                tforms[-1].append(ts.tforms[-1])
-    
-        tids[0] = np.array(tids[0])
-        tids[1] = np.array(tids[1])
-    
-        #use mongo to get the point matches
-        matches = get_matches(z1,z2,self.args['pointmatch'],dbconnection)
-        self.xya,self.xyd = compute_residuals(tids,tforms,matches) 
-        self.rss = np.sqrt(np.power(self.xyd[0,:],2.0)+np.power(self.xyd[1,:],2.0))
-    
-        self.mx = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.xyd[0,:].min(),self.xyd[0,:].max(),self.xyd[0,:].mean(),self.xyd[0,:].std())
-        self.my = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.xyd[1,:].min(),self.xyd[1,:].max(),self.xyd[1,:].mean(),self.xyd[1,:].std())
-        self.mr = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.rss.min(),self.rss.max(),self.rss.mean(),self.rss.std())
+        #get the tilespecs and pointmatches
+        tspecs = [renderapi.tilespec.get_tile_specs_from_z(self.args['output_stack']['name'],float(self.args['z1']),render=stack_dbconnection)]
+        if self.args['z1']!=self.args['z2']:
+            tspecs.append(renderapi.tilespec.get_tile_specs_from_z(self.args['output_stack']['name'],float(self.args['z2']),render=stack_dbconnection))
+        matches = renderapi.pointmatch.get_matches_from_group_to_group(self.args['pointmatch']['name'],
+                                                                     str(float(self.args['z1'])),
+                                                                     str(float(self.args['z2'])),
+                                                                     render=match_dbconnection)
+
+        self.p,self.q,self.p_transf,self.q_transf = transform_pq(tspecs,matches) 
+        self.xy_ave = [0.5*(p+q) for p,q in zip(self.p_transf,self.q_transf)]
+        self.xy_diff = [(p-q) for p,q in zip(self.p_transf,self.q_transf)]
+        self.rss = [np.sqrt(np.power(x[:,0],2.0)+np.power(x[:,1],2.0)) for x in self.xy_diff]
+
+        self.mx=''
+        self.my=''
+        self.mr=''
+
+        #self.rss = np.sqrt(np.power(self.xy_diff[0,:],2.0)+np.power(self.xy_diff[1,:],2.0))
+        #self.mx = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.xy_diff[0,:].min(),self.xy_diff[0,:].max(),self.xy_diff[0,:].mean(),self.xy_diff[0,:].std())
+        #self.my = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.xy_diff[1,:].min(),self.xy_diff[1,:].max(),self.xy_diff[1,:].mean(),self.xy_diff[1,:].std())
+        #self.mr = '[min , max] ave +/- sig\n[%0.1f , %0.1f] %0.1f +/- %0.1f'%(self.rss.min(),self.rss.max(),self.rss.mean(),self.rss.std())
     
         self.ident = 'owner: %s'%self.args['output_stack']['owner']
         self.ident += '\nproject: %s'%self.args['output_stack']['project']
         self.ident += '\nstack: %s'%self.args['output_stack']['name']
         self.ident += '\n'+'collection: %s'%self.args['pointmatch']['name']
-        self.ident += '\n'+'z1,z2: %d,%d'%(z1,z2)
-        print('\n%s'%self.ident)
-        print(self.mx)
-        print(self.my)
-        print(self.mr)
+        self.ident += '\n'+'z1,z2: %d,%d'%(self.args['z1'],self.args['z2'])
+        #print('\n%s'%self.ident)
+        #print(self.mx)
+        #print(self.my)
+        #print(self.mr)
 
-
-    def make_sd_plot(self,ax,choice='rss'):
+    def make_plot(self,ax,coord_choice='xya',color_choice='rss',colorbar=False):
+        #plot against original lens-corrected coordinates
+        sign=1
+        if coord_choice=='xya':
+            plot_coords = np.concatenate(self.xy_ave)
+        elif coord_choice=='p':
+            plot_coords = np.concatenate(self.p)
+        elif coord_choice=='q':
+            plot_coords = np.concatenate(self.q)
+            sign=-1
         cmin = -self.args['threshold']
         cmax = self.args['threshold']
-        if choice=='x':
-            c = self.xyd[0,:]
+        if color_choice=='x':
+            c = sign*np.concatenate(self.xy_diff)[:,0]
             xlab = self.mx
-        elif choice=='y':
-            c = self.xyd[1,:]
+        elif color_choice=='y':
+            c = sign*np.concatenate(self.xy_diff)[:,1]
             xlab = self.my
-        elif choice=='rss':
-            c = self.rss
+        elif color_choice=='rss':
+            c = np.concatenate(self.rss)
             xlab = self.mr
             cmin=0
-
         #function to make the map of the residuals as scatter density plots
         if self.args['density']:
-            density = ax.scatter_density(self.xya[0,:],self.xya[1,:], c=c,cmap=self.cmap)
+            density = ax.scatter_density(plot_coords[:,0],plot_coords[:,1], c=c,cmap=self.cmap)
         else:
-            density = ax.scatter(self.xya[0,:],self.xya[1,:], c=c,cmap=self.cmap,edgecolors=None)
+            density = ax.scatter(plot_coords[:,0],plot_coords[:,1], c=c,cmap=self.cmap,edgecolors=None)
+
         ax.set_aspect('equal')
         ax.patch.set_color([0.5,0.5,0.5])
         ax.set_xticks([])
@@ -132,23 +124,41 @@ class CheckResiduals(argschema.ArgSchemaParser):
         ax.invert_yaxis() #match fine stack in ndviz
         ax.set_xlabel(xlab)
         fig = plt.gcf()
-        fig.colorbar(density)
+        if colorbar:
+            fig.colorbar(density)
+
         if self.args['threshold'] is not None:
             density.set_clim(cmin,cmax)
 
+    def make_lc_plots(self,fig):
+        fig.clf()
+        ax1=fig.add_subplot(141,projection='scatter_density')
+        self.make_plot(ax1,coord_choice='p',color_choice='x',colorbar=True)
+        ax1.set_title('$\Delta x$ as p')
+        ax2=fig.add_subplot(142,projection='scatter_density')
+        self.make_plot(ax2,coord_choice='q',color_choice='x',colorbar=True)
+        ax2.set_title('$\Delta x$ as q')
+        ax3=fig.add_subplot(143,projection='scatter_density')
+        self.make_plot(ax3,coord_choice='p',color_choice='y',colorbar=True)
+        ax3.set_title('$\Delta y$ as p')
+        ax4=fig.add_subplot(144,projection='scatter_density')
+        self.make_plot(ax4,coord_choice='q',color_choice='y',colorbar=True)
+        ax4.set_title('$\Delta y$ as q')
+
+
     def make_plots(self):
-        fig = plt.figure(1,figsize=(40,7.5))
+        fig = plt.figure(1,figsize=(12,7.5))
         fig.clf()
 
         #x residuals
         ax1 = fig.add_subplot(1,3,1,projection='scatter_density')
-        self.make_sd_plot(ax1,choice='x')
+        self.make_plot(ax1,coord_choice='xya',color_choice='x')
         #y residuals
         ax2 = fig.add_subplot(1,3,2,projection='scatter_density')
-        self.make_sd_plot(ax2,choice='y')
+        self.make_plot(ax2,coord_choice='xya',color_choice='y')
         #rss residuals
         ax3 = fig.add_subplot(1,3,3,projection='scatter_density')
-        self.make_sd_plot(ax3,choice='rss')
+        self.make_plot(ax3,coord_choice='xya',color_choice='rss')
     
         ax1.set_title(self.ident+'\n$\Delta x$',fontsize=10)
         ax2.set_title(self.ident+'\n$\Delta y$',fontsize=10)
@@ -168,8 +178,6 @@ class CheckResiduals(argschema.ArgSchemaParser):
            print('wrote %s'%self.outputname)
 
 if __name__=='__main__':
-    t0 = time.time()
     mod = CheckResiduals(schema_type=EMA_PlotSchema)
     mod.run()
-    print('total time: %0.1f'%(time.time()-t0))
    

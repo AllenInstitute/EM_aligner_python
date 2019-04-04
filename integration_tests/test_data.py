e920871d482acff291ab4b2fb6cc08f843c94526
Dan Kapner <danielk@.alleninstitute.org>
2019-04-03 21:44:10 -0700

    upping java heap

--- integration_tests/test_data.py
+++ integration_tests/test_data.py
@@ -34,7 +34,8 @@ render_params = {
     'port': render_port,
     'owner': 'test',
     'project': project,
-    'client_scripts': client_script_location
+    'client_scripts': client_script_location,
+    'memGB': '4G'
 }
 
 montage_raw_tilespecs_json = render_json_template(

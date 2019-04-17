013a0005b3eaf12eb8a556852df6543cbd1ab7f0
Dan Kapner <danielk@.alleninstitute.org>
2019-04-17 14:52:54 -0700

    documenting jsongz

--- EMaligner/jsongz.py
+++ EMaligner/jsongz.py
@@ -20,6 +20,31 @@ def _check_ext(filepath):
 
 
 def dump(obj, filepath, compress=None, encoding='utf-8', *args, **kwargs):
+    """json or json.gz dump
+
+    Parameters
+    ----------
+    obj : obj
+        object to dump
+    filepath : str
+        path for destination of dump
+    compress : bool or None
+        if None, file compressed or not according to filepath extension
+    encoding : str
+        encoding of json.dumps() before writing to .gz file.
+        or encoding passed into json.dump()
+    *args
+        json.dump args
+    **kwargs
+        json.dump kwargs
+
+    Returns
+    -------
+    filepath : str
+        potentially modified filepath of dumped object
+        uncompressed are forced to '.json' and compressed to '.gz'
+    """
+
     if not compress:
         compress = _check_ext(filepath)
     filepath = _convert_ext(filepath, compress)
@@ -28,16 +53,37 @@ def dump(obj, filepath, compress=None, encoding='utf-8', *args, **kwargs):
             f.write(json.dumps(obj, *args, **kwargs).encode(encoding))
     else:
         with open(filepath, 'w') as f:
-            json.dump(obj, f, *args, **kwargs)
+            json.dump(obj, f, encoding=encoding, *args, **kwargs)
     return filepath
 
 
 def load(filepath, encoding='utf-8', *args, **kwargs):
+    """json or json.gz load
+
+    Parameters
+    ----------
+    filepath : str
+        path for source of load
+    encoding : str
+        encoding for decoding of json.dumps() after .gz read
+        or encoding passed into json.load()
+    *args
+        json.load args
+    **kwargs
+        json.load kwargs
+
+    Returns
+    -------
+    obj : obj
+        potentially modified filepath of dumped object
+        uncompressed are forced to '.json' and compressed to '.gz'
+    """
+
     compressed = _check_ext(filepath)
     if compressed:
         with gzip.GzipFile(filepath, 'r') as f:
             obj = json.loads(f.read().decode(encoding), *args, **kwargs)
     else:
         with open(filepath, 'r') as f:
-            obj = json.load(f, *args, **kwargs)
+            obj = json.load(f, encoding=encoding, *args, **kwargs)
     return obj

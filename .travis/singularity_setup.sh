8ce999397d10276479c50460981fa18f5fd4c1a4
Dan Kapner <danielk@.alleninstitute.org>
2019-05-10 04:50:04 -0700

    tweaking singularity build

--- .travis/singularity_setup.sh
+++ .travis/singularity_setup.sh
@@ -3,7 +3,7 @@
 # install go
 export VERSION=1.11 OS=linux ARCH=amd64
 wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
@@ -25,14 +25,25 @@ echo "sregistry Version:"
 
 # Install Singularity
 
-SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
-export PATH="${GOPATH}/bin:${PATH}"
-
-mkdir -p "${GOPATH}/src/github.com/sylabs"
-cd "${GOPATH}/src/github.com/sylabs"
-
-git clone -b release-3.2 https://github.com/sylabs/singularity
+export VERSION= 3.1.1 && # adjust this as necessary
+mkdir -p $GOPATH/src/github.com/sylabs
+cd $GOPATH/src/github.com/sylabs
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
 cd singularity
-./mconfig -v -p /usr/local
-make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
+
+./mconfig
+make -C ./builddir
 sudo make -C ./builddir install
+
+#SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
+#export PATH="${GOPATH}/bin:${PATH}"
+#
+#mkdir -p "${GOPATH}/src/github.com/sylabs"
+#cd "${GOPATH}/src/github.com/sylabs"
+#
+#git clone -b release-3.2 https://github.com/sylabs/singularity
+#cd singularity
+#./mconfig -v -p /usr/local
+#make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
+#sudo make -C ./builddir install

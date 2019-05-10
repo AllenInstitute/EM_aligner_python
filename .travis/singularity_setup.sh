e684bae8b49886b8ddce16beb331b271d9b09dd5
Dan Kapner <danielk@.alleninstitute.org>
2019-05-10 04:34:32 -0700

    trying to fix singularity install

--- .travis/singularity_setup.sh
+++ .travis/singularity_setup.sh
@@ -18,7 +18,7 @@ sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 
 echo "Python Version:"
 python --version
-pip install --user sregistry[all]
+pip install sregistry[all]
 sregistry version
 
 echo "sregistry Version:"

7ba7a92ff1b724f34c6e4f99017b060e266e2b7c
Dan Kapner <danielk@.alleninstitute.org>
2019-06-07 08:25:33 -0700

    delete A and then reload it from disk after solve

--- EMaligner/distributed/src/em_dist_solve.c
+++ EMaligner/distributed/src/em_dist_solve.c
@@ -211,6 +211,8 @@ main (int argc, char **args)
   ierr = MatMatMult (ATW, A, MAT_INITIAL_MATRIX, PETSC_DEFAULT, &K);
   CHKERRQ (ierr);
 
+  MatDestroy (&A);
+
   /*  find out how the rows are distributed   */
   MatGetOwnershipRange (K, &local_row0, &local_rowN);
   MatGetSize (K, &global_nrow, NULL);
@@ -260,7 +262,9 @@ main (int argc, char **args)
       CHKERRQ (ierr);
       ierr = VecAXPY (Lm[i], 1.0, ATWRHS[i]);
       CHKERRQ (ierr);
+      VecDestroy (&ATWRHS[i]);
     }
+
   if (rank == 0)
     {
       printf ("Lm(s) created\n");
@@ -350,6 +354,38 @@ main (int argc, char **args)
   strcat (strout, "\n");
   strcat (results_out, "],");
 
+  MatDestroy (&K);
+
+  // we want A again. 
+  /*  allocate space for local CSR arrays  */
+  local_indptr = (PetscInt *) calloc (local_nrow + 1, sizeof (PetscInt));
+  local_jcol = (PetscInt *) calloc (local_nnz, sizeof (PetscInt));
+  local_data = (PetscScalar *) calloc (local_nnz, sizeof (PetscScalar));
+  local_weights = (PetscScalar *) calloc (local_nrow, sizeof (PetscScalar));
+  local_rhs = (PetscScalar **) malloc (nsolve * sizeof (PetscInt *));
+  for (i = 0; i < nsolve; i++)
+    {
+      local_rhs[i] =
+	(PetscScalar *) calloc (local_nrow, sizeof (PetscScalar));
+    }
+  /*  read in local hdf5 files and concatenate into CSR arrays  */
+  ierr =
+    ReadLocalCSR (PETSC_COMM_SELF, csrnames, local_firstfile, local_lastfile,
+		  nsolve, local_indptr, local_jcol, local_data, local_weights,
+		  local_rhs);
+  CHKERRQ (ierr);
+  /*  Create distributed A!  */
+  MatCreateMPIAIJWithArrays (PETSC_COMM_WORLD, local_nrow, PETSC_DECIDE,
+  			     global_nrow, global_ncol, local_indptr,
+  	                     local_jcol, local_data, &A);
+  free (local_jcol);
+  free (local_data);
+  free (local_indptr);
+  if (rank == 0)
+    {
+      printf ("A matrix created for error calculation\n");
+    }
+
   PetscInt mA, nA, c0, cn;
   ierr = MatGetSize (A, &mA, &nA);
   CHKERRQ (ierr);
@@ -435,10 +471,8 @@ main (int argc, char **args)
       VecDestroy (&x[i]);
       VecDestroy (&x0[i]);
       VecDestroy (&rhs[i]);
-      VecDestroy (&ATWRHS[i]);
     }
   MatDestroy (&A);
-  MatDestroy (&K);
   MatDestroy (&ATW);
   KSPDestroy (&ksp);
   free (dir);
